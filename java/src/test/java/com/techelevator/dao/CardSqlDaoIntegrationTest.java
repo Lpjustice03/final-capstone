736ca57413f41803f66a4dcb19c7ca180408fe81
Lance Justice <lpjustice03@gmail.com>
2020-08-05 12:06:08 -0400

    update backend

---
+++ java/src/test/java/com/techelevator/dao/CardSqlDaoIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.techelevator.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+import com.techelevator.model.Card;
+
+import org.junit.Assert;
+
+public class CardSqlDaoIntegrationTest extends DAOIntegrationTest{
+	private CardSqlDAO cardSqlDAO;
+
+    @Before
+    public void setup() {
+        DataSource dataSource = this.getDataSource();
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        cardSqlDAO = new CardSqlDAO(jdbcTemplate);
+    }
+    
+    @Test
+    public void getCardTest() {
+    	List<Card> cards = new ArrayList<>();
+    	
+    	boolean isEquals = false;
+    	
+    	cards = cardSqlDAO.getCards();
+    	if (cards.get(0).getId() == 1) {
+    		isEquals = true;
+    	}
+    	assertEquals(true, isEquals);
+    }
+
+}
