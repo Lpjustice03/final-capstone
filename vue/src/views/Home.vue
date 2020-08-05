8078a848a921e917836fd07d08eb072b1d9b7332
Lance Justice <lpjustice03@gmail.com>
2020-08-05 19:23:53 -0400

    update

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,27 +2,28 @@
   <div class="home">
 
 
-    <the-header />
+    <h1>FlipDeck</h1>
+    <h2> Welcome to FlipDeck&#169; the coolest study tool around</h2>
+    <h3> makes simple learning tools that let you study anything. Start learning today with flashcards, games and learning tools — all for free!</h3>
+
     
-      
-  
-  <!-- <footer> Facebook icon, IG icon, etc... </footer> -->
 
     
     
     
   </div>
+   <!-- <footer> Facebook icon, IG icon, etc... </footer> -->
 </template>
 
 <script>
 
 
-import TheHeader from "@/components/TheHeader.vue"
+
 
 export default {
   name: "Home",
   components: {
-    TheHeader
+    
    
    
   }
@@ -31,4 +32,35 @@ export default {
 
 };
 </script>
+
+<style scoped>
+
+.home{
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  opacity: 0.8;
+  background: url(../assets/heroImage.jpg)left / cover no-repeat;
+  display: flex;
+}
+
+ h1, h2, h3 {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 60px;
+  display: flex;
+  align-content:bottom;
+}
+
+
+
+
+
+
+
+
+
+
+</style>
 
