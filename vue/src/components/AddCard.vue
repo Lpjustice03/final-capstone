2813bbad715835e306ad2ac7b9b74cdf90d7c102
Lance Justice <lpjustice03@gmail.com>
2020-08-06 19:11:56 -0400

    update

--- vue/src/components/AddCard.vue
+++ vue/src/components/AddCard.vue
@@ -24,21 +24,32 @@ import cardService from "@/services/CardService.js";
 
 export default {
     name: "add-card",
+    
     data() {
         return{
         card:{
             
             front: '',
             back: '',
-            deckId: 1,
-            userId: 2
+            userId: '',
+            deckId: ''
+            
         }
         };
 
     },
     methods: {
         saveCard() {
-            cardService.addCard(this.card)
+            const newCard ={
+                front: this.card.front,
+                back: this.card.back,
+                userId: this.card.userId,
+                deckId: this.card.deckId,
+                cardCreatorId: this.card.userId
+
+            };
+           
+            cardService.addCard(newCard)
             // .then((response) => {
             //     if (response.status === 201) {
             //         this.$router.push(this.params.deckId);
