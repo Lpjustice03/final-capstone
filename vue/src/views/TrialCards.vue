aa8f6a08ab7aebf4cba7f0cf70c7fd8716dd3274
Lance Justice <lpjustice03@gmail.com>
2020-08-07 18:43:09 -0400

    Lance mispleeeing errorroer

---
+++ vue/src/views/TrialCards.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="trialCards">
+<card-trial v-bind:deck-id="parseInt($route.params.id)" />
+
+</div>
+  
+</template>
+
+<script>
+
+import CardTrial from '@/components/CardTrial.vue';
+
+export default {
+    name: "TrialCards",
+    components: {
+    CardTrial
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
