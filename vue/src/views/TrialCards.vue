dca0e4322c8e902cd9b705ffc497d00a15f82fe4
Michael Finegan <mfinegan96@gmail.com>
2020-08-11 18:18:11 -0400

    updated trial cards

--- vue/src/views/TrialCards.vue
+++ vue/src/views/TrialCards.vue
@@ -1,7 +1,13 @@
 <template>
 <div class="trialCards">
+        <h1 class="title"> Cardz </h1>
+    <timer />
+    <div class="cardsBackground">
 <card-trial v-bind:deck-id="parseInt($route.params.id)" />
-
+</div>
+<div class="button">
+<router-link class="moreDecks" id="decks" tag="button" v-bind:to="{name:'TrialPage'}"> Try another deck! </router-link> 
+</div>
 </div>
   
 </template>
@@ -9,11 +15,13 @@
 <script>
 
 import CardTrial from '@/components/CardTrial.vue';
+import Timer from '@/components/Timer.vue';
 
 export default {
     name: "TrialCards",
     components: {
-    CardTrial
+    CardTrial,
+    Timer
 
     }
 
@@ -22,8 +30,37 @@ export default {
 
 <style>
 .trialCards {
-   
-    background-color: #DCC7AA;
-    min-height: 90vh;
+  background-color: rgb(214, 182, 137);
+  margin-top: -34px;
+
 }
+
+.title {
+  font-size: 50px;
+  text-align: center;
+  color: white;
+  background-color: rgb(214, 182, 137);
+}
+
+.cardsBackground{
+    background-color: #DCC7AA;
+}
+
+.button{
+    background-color: #DCC7AA;
+    display: flex;
+    justify-content: flex-end;
+}
+
+.moreDecks{
+    background-color: #F7882F;
+    color: #F7C331;
+    height: 50px;
+}
+
+.moreDecks:hover{
+    background-color: #F7C331;
+    color: #F7882F;
+}
+
 </style>
